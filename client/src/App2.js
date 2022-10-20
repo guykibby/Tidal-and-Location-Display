c79c3593b16986a9233c99f182b688c86ad4f540
Guy Kibby <GuyKibby@gmail.com>
2022-10-21 11:27:32 +1300

    add async await for data fetch

---
+++ client/src/App2.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+
+function App2() {
+  const [tidalArray, setTidalArray] = useState([
+    0, 0.707, 1, 0.707, 0, -0.707, -1, -0.707, 0, 0.707, 1, 0.707, 0, -0.707,
+    -1, -0.707,
+  ]);
+
+  useEffect(() => {
+    const unixDate = Date.now();
+
+    const incOfDay = 48;
+    const timeInc = (24 * 60 * 60 * 1000) / incOfDay;
+    // console.log(timeInc);
+    let newArray = [];
+
+    const fetchData = async (date) => {
+      const response = await fetch(
+        `https://rain-mxm.begin.app/water-depth?lat=-36.1&long=174.1&date=${date}`
+      );
+      const data = await response.json();
+      console.log(data.data.waterDepth);
+      newArray.push(data.data.waterDepth);
+    };
+
+    for (let i = 0; i <= incOfDay; i++) {
+      const incUnixDate = unixDate + timeInc * i;
+      const incIsoDate = new Date(incUnixDate).toISOString();
+      fetchData(incIsoDate);
+    }
+    console.log(newArray);
+
+    setTidalArray(newArray);
+  }, []);
+
+  return (
+    <div className="tidalData">
+      {tidalArray.map((e, index) => (
+        <p key={index}>{e}</p>
+      ))}
+    </div>
+  );
+}
+
+export default App2;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App1 from "./App1";
+// import App1 from "./App1";
 import App2 from "./App2.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App1 />
+    {/* <App1 /> */}
     <App2 />
   </React.StrictMode>,
   document.getElementById("root")
