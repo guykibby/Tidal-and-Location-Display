869e3c895dfd8dae57c253fb0f23d7e1ed186c3e
Guy Kibby <GuyKibby@gmail.com>
2022-10-21 09:45:51 +1300

    add app files

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App1 from "./App1";
+import App2 from "./App2";
 
 ReactDOM.render(
   <React.StrictMode>
     <p>kia ora</p>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
