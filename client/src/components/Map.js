c80b8c155f29af38bbc67f9526bfbbd46ece3a6a
Ching-wen Hu <ching-wen.hu@developersinstitute.co.nz>
2022-10-21 14:49:51 +1300

    Set up the initial Map display

---
+++ client/src/components/Map.js
@@ -0,0 +1,26 @@
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+export default function Home() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyCecPt-U0pcXsQ47red7hiKTDU1LQ1auvE",
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
+}
+
+const containerStyle = {
+  width: "100%",
+  height: "500px",
+};
+
+function Map() {
+  const center = useMemo(() => ({ lat: -36.848461, lng: 174.763336 }), []);
+
+  return (
+    <GoogleMap zoom={10} center={center} mapContainerStyle={containerStyle}>
+      <Marker position={center} />
+    </GoogleMap>
+  );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import App2 from "./App2";
 ReactDOM.render(
   <React.StrictMode>
     <p>kia ora</p>
+    <App1 />
   </React.StrictMode>,
   document.getElementById("root")
 );
