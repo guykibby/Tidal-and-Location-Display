be60d75207409104e6445315867cbe70ca89b459
olle1134 <99058610+olle1134@users.noreply.github.com>
2022-10-21 17:00:09 +1300

    updated css.

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App1 from "./App1";
+import App1 from "./App1";
 import App2 from "./App2.js";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App1 />
-    <App2 />
+    <div className="container">
+      <div className="map">
+        <App1 />
+      </div>
+
+      <App2 />
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
